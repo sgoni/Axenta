cb8df1077325d857ba28e95afd078ccd5abeb6bd
s.goni <steven.goni@outlook.com>
2025-07-08 17:55:49 -0600

    Add .gitignore file

---
+++ src/backend/src/Services/Accounting/Accounting.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Accounting.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    // DbSets
+    public DbSet<AccountType> AccountTypes => Set<AccountType>();
+    public DbSet<Account> Accounts => Set<Account>();
+    public DbSet<Period> Periods => Set<Period>();
+    public DbSet<JournalEntry> JournalEntries => Set<JournalEntry>();
+    public DbSet<JournalEntryLine> JournalEntryLines => Set<JournalEntryLine>();
+    public DbSet<DocumentReference> DocumentReferences => Set<DocumentReference>();
+    public DbSet<AuditLog> AuditLogs => Set<AuditLog>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
