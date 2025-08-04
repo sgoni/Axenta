df5c4c5788edf00e0b2371aec8c6bdc52681d7d1
s.goni <steven.goni@outlook.com>
2025-08-03 19:51:04 -0600

    Creacion de proyecto de test Axenta.Test

---
+++ src/backend/src/tests/Axenta.Tests/Domain/JournalEntryTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Axenta.Tests.Domain;
+
+public class JournalEntryTests
+{
+    [Fact]
+    public void Creating_balanced_entry_succeeds()
+    {
+    }
+
+    [Fact]
+    public void Creating_unbalanced_entry_throws()
+    {
+    }
+}
