3d22d99d30ddad646a72485de89ddf35f9c2dab0
s.goni <steven.goni@outlook.com>
2025-08-09 16:14:18 -0600

    Develop AccountingPolicyTemplate domain class

---
+++ src/backend/src/Services/Accounting/Accounting.Domain/Models/AccountingPolicyTemplate.cs
@@ -0,0 +1,29 @@
+﻿namespace Accounting.Domain.Models;
+
+public class AccountingPolicyTemplate : Entity<AccountingPolicyTemplateId>
+{
+    private readonly List<AccountingPolicyLine> _lines = new();
+    public IReadOnlyCollection<AccountingPolicyLine> Lines => _lines.AsReadOnly();
+
+    public string Name { get; private set; }
+    public string Trigger { get; private set; } // Ej: "MonthEnd", "InvoicePosted"
+
+    public static AccountingPolicyTemplate Create(AccountingPolicyTemplateId id, string name, string trigger)
+    {
+        var accountingPolicyTemplate = new AccountingPolicyTemplate
+        {
+            Id = id,
+            Name = name,
+            Trigger = trigger,
+        };
+
+        return accountingPolicyTemplate;
+    }
+
+    public void AddLine(AccountId accountId, decimal debit, decimal credit)
+    {
+        _lines.Add(new AccountingPolicyLine(accountId, debit, credit));
+    }
+}
+
+public record AccountingPolicyLine(AccountId AccountId, decimal Debit, decimal Credit);
---
+++ src/backend/src/Services/Accounting/Accounting.Domain/VelueObjects/AccountingPolicyTemplateId.cs
@@ -0,0 +1,19 @@
+﻿namespace Accounting.Domain.VelueObjects;
+
+public record AccountingPolicyTemplateId
+{
+    private AccountingPolicyTemplateId(Guid value)
+    {
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public static AccountingPolicyTemplateId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty) throw new DomainException("AccountingPolicyTemplateId cannot be empty");
+
+        return new AccountingPolicyTemplateId(value);
+    }
+}
