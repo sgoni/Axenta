71f572c7e5c5d45c4afc5ba6a1915b45eca22c9b
s.goni <steven.goni@outlook.com>
2025-08-10 13:55:51 -0600

    Develop class domain Company

---
+++ src/backend/src/Services/Accounting/Accounting.Domain/Models/Company.cs
@@ -0,0 +1,40 @@
+﻿namespace Accounting.Domain.Models;
+
+public class Company : Aggregate<CompanyId>
+{
+    public string Name { get; private set; }
+    public string TaxId { get; private set; }
+    public string Country { get; private set; }
+    public string CurrencyCode { get; private set; }
+    public bool IsActive { get; private set; } = true;
+
+    public static Company Create(CompanyId id, string name, string taxId, string country, string currencyCode)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(taxId);
+        ArgumentException.ThrowIfNullOrWhiteSpace(currencyCode);
+
+        var company = new Company
+        {
+            Id = id,
+            Name = name,
+            TaxId = taxId,
+            Country = country,
+            IsActive = true
+        };
+
+        return company;
+    }
+
+    public void Update(string name, string taxId, string country, string currencyCode, bool isActive)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(taxId);
+        ArgumentException.ThrowIfNullOrWhiteSpace(currencyCode);
+
+        Name = name;
+        TaxId = taxId;
+        Country = country;
+        IsActive = isActive;
+    }
+}
---
+++ src/backend/src/Services/Accounting/Accounting.Domain/VelueObjects/CompanyId.cs
@@ -0,0 +1,19 @@
+﻿namespace Accounting.Domain.VelueObjects;
+
+public record CompanyId
+{
+    private CompanyId(Guid value)
+    {
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public static CompanyId Of(Guid value)
+    {
+        if (value == Guid.Empty)
+            throw new DomainException("CompanyId cannot be empty");
+
+        return new CompanyId(value);
+    }
+}
