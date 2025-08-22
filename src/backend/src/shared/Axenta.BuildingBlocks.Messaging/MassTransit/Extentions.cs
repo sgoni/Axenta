f11e520833964cc575a3f3e560371442c011eac1
s.goni <steven.goni@outlook.com>
2025-08-22 17:06:41 -0600

    Axenta.BuildingBlocks.Messaging

---
+++ src/backend/src/shared/Axenta.BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Axenta.BuildingBlocks.Messaging.MassTransit;
+
+public static class Extentions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration,
+        Assembly? assembly = null)
+    {
+        // Implement RabbitMQ MassTransit configuration
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null)
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:Username"]);
+                    host.Password(configuration["MessageBroker:Password"]);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+        
+        return services;
+    }
+}
