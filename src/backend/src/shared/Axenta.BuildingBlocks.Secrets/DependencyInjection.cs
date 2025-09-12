71466c3815bba53f9a1b3650a28dc73faddcd2bb
s.goni <steven.goni@outlook.com>
2025-09-11 19:29:00 -0600

    Transversal Concern Projects

---
+++ src/backend/src/shared/Axenta.BuildingBlocks.Secrets/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Axenta.BuildingBlocks.Secrets;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddVault(this IServiceCollection services, IConfiguration configuration)
+    {
+        if (services == null) throw new ArgumentNullException(nameof(services));
+
+        // Cargar la configuración de Vault desde appsettings.json
+        services.Configure<VaultSettings>(configuration.GetSection("VaultSettings"));
+
+        // Registrar la clase VaultConfigurationProvider como un servicio
+        services.AddScoped<ISecretManager, VaultConfigurationProvider>();
+
+        return services;
+    }
+}
